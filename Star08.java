528c84a75d9d609085d0a8cebe4defd153fc7924
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-28 17:27:50 +0700

    membuat masukkan nilai menggunakan scanner int dan membuat perulangan for

--- Star08.java
+++ Star08.java
@@ -4,7 +4,12 @@ public class Star08 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-         
+        System.out.println("Masukkan nilai n =");
+         int n = sc.nextInt();
+
+         for (int i=1; i<=n; i++) {
+            System.out.print("*");
+         } 
         
     }
 }
