268f07323198a3b9f798dd3881c3c94b910e55f9
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-31 22:14:33 +0700

    membuat inputan nya

--- Square08.java
+++ Square08.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class Square08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
     }
 }
