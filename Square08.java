4a614a8ef2741f0a119bbabf8a8964186ab9ff0f
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-10-31 22:37:57 +0700

    perubahan yang tadi salah pada system.out.println seharusnya pakai system.out.print

--- Square08.java
+++ Square08.java
@@ -3,14 +3,13 @@ import java.util.Scanner;
 public class Square08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        System.out.println("Masukkan nilai N = ");
         int N = sc.nextInt();
-
-        for (int iOuter = 1; iOuter<=N; iOuter ++) {
-            for (int i = 1; i<=N; i++) {
-            System.out.println("*");
-            }
-            System.out.println();
+        for(int iOuter=1; iOuter<=N; iOuter++) {
+            for (int i=1; i<=N; i++) {
+                System.out.print("*");
+        }
+            System.out.println("");
         }
     }
 }
