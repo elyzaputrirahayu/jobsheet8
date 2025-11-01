ae88c4ff87d80e62e9f4e25c4088ee826c09669f
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-01 11:53:59 +0700

    modifikasi innner loop nya yaitu mengubah i=1 menjadi i=0

--- Square08.java
+++ Square08.java
@@ -5,7 +5,7 @@ public class Square08 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan nilai N = ");
         int N = sc.nextInt();
-        for(int iOuter=0; iOuter<=N; iOuter++) {
+        for(int iOuter=1; iOuter<=N; iOuter++) {
             for (int i=0; i<=N; i++) {
                 System.out.print("*");
         }
