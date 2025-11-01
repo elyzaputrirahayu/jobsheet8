a7f87a5989930527f5fe7ef0972bd335b5b9cc68
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-01 12:33:04 +0700

    membuat inputan

--- Triangle08.java
+++ Triangle08.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Triangle08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan nilai n = ");
+        int n = sc.nextInt();
+        int i = 0;
 
          
     }
