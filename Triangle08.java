de454c058a6761ecd2f75670431423196d1c9f5e
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-01 12:41:41 +0700

    percobaan 3

--- Triangle08.java
+++ Triangle08.java
@@ -4,6 +4,7 @@ public class Triangle08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan nilai n = ");
+        
         int n = sc.nextInt();
         int i = 0;
 
@@ -14,7 +15,6 @@ public class Triangle08 {
                 j++;
             }
             i++;
-            
         }
 
     }
