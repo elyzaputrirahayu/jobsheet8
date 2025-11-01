85892641de878cd004ea2a1ec241c4776f53ae82
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-01 12:09:03 +0700

    modifikasi menghilangkan system.out.print nya

--- Square08.java
+++ Square08.java
@@ -6,8 +6,7 @@ public class Square08 {
         System.out.println("Masukkan nilai N = ");
         int N = sc.nextInt();
         for(int iOuter=1; iOuter<=N; iOuter++) {
-            for (int i=0; i<=N; i++) {
-                System.out.print("*");
+            for (int i=1; i<=N; i++) {
         }
             System.out.println("");
         }
