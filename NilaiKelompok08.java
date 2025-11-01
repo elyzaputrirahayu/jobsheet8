9c85b831a4a49622c40b9e73ec09b45309ee1584
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-01 16:07:29 +0700

    membuat struktur perulangan outer loop menggunakan while

--- NilaiKelompok08.java
+++ NilaiKelompok08.java
@@ -4,11 +4,22 @@ public class NilaiKelompok08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int i, j, nilai;
+        int nilai;
         float totalNilai,rataNilai;
 
-        for (j=1;j<=5;j++) {
-            
+        int i=1;
+        while (i <= 6) {
+            System.out.println("Kelompok " + i);
+        
+            totalNilai = 0;
+            for ( int j = 1;j<=5;j++) {
+                System.out.print("Nilai dari kelompok Penilai " + j + ":");
+                nilai = sc.nextInt();
+                totalNilai += nilai;
+            }
+
+            rataNilai=totalNilai / 5;
+            i++;
         }
     }
 }
