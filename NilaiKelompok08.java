edd3aa69523a79a87b75344a2704559faf9ad725
elyzaputrirahayu <eliscantik2000@gmail.com>
2025-11-01 15:31:21 +0700

    membuat deklarasi

--- NilaiKelompok08.java
+++ NilaiKelompok08.java
@@ -4,6 +4,9 @@ public class NilaiKelompok08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int i, j, nilai;
+        float totalNilai,rataNilai;
+
         
     }
 }
